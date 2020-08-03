b49c65317b12b7defd65f2bbd99850b208a34f1f
Matt Mason <masonmj10@gmail.com>
2020-08-03 05:02:51 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,6 +6,14 @@
 class EmailParser 
   attr_accessor :emails 
   
+  def initialize(emails)
+    self.emails = emails
+  end 
+  
+  def parse 
+    self.emails.split(/,\s|\s/).uniq
+  end 
+  
   
 end 
 
